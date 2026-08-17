fb8d0cb86f294e2d287a006b7c6ab7b0b33adf2d
shoemajor <shoefanaticapp@gmail.com>
2026-08-17 13:38:54 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='penpicks-v3';
-const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return;}
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
